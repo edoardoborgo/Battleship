152715759d86825ddb8517660826dca0e0448f00
G1ANK0 <giovanni.giancola@studenti.unipd.it>
2023-01-11 18:23:41 +0100

    Changes in grill by JNKS 11-1-2023 6:24pm

--- utilities/main.cpp
+++ utilities/main.cpp
@@ -4,40 +4,25 @@
 using namespace std;
 
 int main(){
-        // Initializing the vector of vectors
-        vector<vector<int> > vec;
-
-        // Elements to insert in column
-        int num = 10;
-
-        // Inserting elements into vector
-        for (int i = 0; i < ROW; i++) {
-            // Vector to store column elements
-            vector<int> v1;
-
-            for (int j = 0; j < COL; j++) {
-                v1.push_back(num);
-                num += 5;
-            }
-
-            // Pushing back above 1D vector
-            // to create the 2D vector
-            vec.push_back(v1);
-        }
-
-        // Displaying the 2D vector
-        for (int i = 0; i < vec.size(); i++) {
-            for (int j = 0; j < vec[i].size(); j++)
-                cout << vec[i][j] << " ";
-            cout << endl;
-        }
-        return 0;
+  int righe = 12;
+  int colonne = 12;
+  std::vector<std::vector<char>> tabellone;
+  char c = ' ';
+  for (int col = 0; col < colonne; col++) {
+    std::vector<char> v_righe;
+    for (int row = 0; row < righe; row++) {
+      v_righe.push_back(c);
     }
-    /*
-    Produzione:
-    10 15 20 25 30
-    35 40 45 50 55
-    60 65 70 75 80
-    85 90 95 100 105
-    */
+    tabellone.push_back(v_righe);
+  }
+
+  cout << "-------------------------" << endl;
+  for (int i = 0; i < tabellone.size(); i++){
+    cout << '|';
+      for(int j = 0; j < tabellone[i].size(); j++){
+        cout << tabellone[i][j] << '-';
+      }
+    cout << endl;
+  }
+  cout << "-------------------------" << endl;
 }
