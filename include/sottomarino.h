3f340b54e6326bb4cbe18ab512203f90c3a5d720
edoardoborgo <borgoedoardo@gmail.com>
2023-01-19 21:22:54 +0100

    update

--- include/sottomarino.h
+++ include/sottomarino.h
@@ -6,12 +6,18 @@
 #define BATTLESHIP_SOTTOMARINO_H
 
 class Sottomarino : public Nave {
- private:
-  //char simbolo_ = 'E';
-  //int dimensione_ = 1;
- public:
-  Sottomarino(battaglia_navale::Coordinate &prua);
-  void azione(Giocatore* attaccante, Giocatore* difensore, battaglia_navale::Coordinate &target);
+public:
+    /*
+     * @prief: costruttore di default di sottomarino
+     */
+    Sottomarino(){}
+    /*
+     * @brief: 
+     *
+     * @param:
+     */
+    explicit Sottomarino(battaglia_navale::Coordinate &prua);
+    void azione(Giocatore* attaccante, Giocatore* difensore, battaglia_navale::Coordinate &target) override;
   void scan_(Giocatore* difensore, Giocatore* attaccante, battaglia_navale::Coordinate &target);
   bool muovi(battaglia_navale::Coordinate &target, Giocatore* attaccante);
   void modifica_range(battaglia_navale::Coordinate &start_scan, battaglia_navale::Coordinate &finish_scan);
